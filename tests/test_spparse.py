c03716f930771a7d74b5185fe8177726a1a5bb38
Andrew Tribick <ajtribick@googlemail.com>
2022-02-19 12:46:53 +0100

    More spectral type parser tests

--- tests/test_spparse.py
+++ tests/test_spparse.py
@@ -16,6 +16,11 @@ def test_spparse_normal():
     assert parse_spectrum("O3- 0-Ia") == 0x0030
     assert parse_spectrum("O3+ 0-Ia") == 0x0030
     assert parse_spectrum("B 0.5IA") == 0x0101
+    assert parse_spectrum("F5V Fe-0.5") == 0x0356
+    assert parse_spectrum("F6/7IV/Vn") == 0x0365
+    assert parse_spectrum("G0.  IA     E,V") == 0x0401
+    assert parse_spectrum("dM1.5e") == 0x0616
+    assert parse_spectrum("sdO2VIII He6") == 0x0027
     assert parse_spectrum("") == 0x0ca8
 
 
@@ -33,6 +38,7 @@ def test_spparse_white_dwarf():
     assert parse_spectrum("wd") == 0x16a0
     assert parse_spectrum("WD") == 0x16a0
     assert parse_spectrum("DC6") == 0x1260
+    assert parse_spectrum("DAe") == 0x10a0
 
 
 def test_spparse_carbon():
@@ -42,6 +48,7 @@ def test_spparse_carbon():
     assert parse_spectrum("C2,4J") == 0x0f28
     assert parse_spectrum("C3,4Hd III") == 0x0f34
     assert parse_spectrum("S4/3 II") == 0x0843
+    assert parse_spectrum("S3+/1- III:") == 0x0834
 
 
 def test_spparse_composite():
